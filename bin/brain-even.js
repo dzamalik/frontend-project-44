073cff84bdc6e10d0456cecf9310c0a958b005fa
HAYTALE <JohnDaveHarrys994@gmail.com>
2026-04-06 12:03:37 +0700

    add

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -8,3 +8,17 @@ export default function app() {
 }
 
 app()
+ 
+  
+function getRandomInt(max) {
+  return Math.floor(Math.random() * max);
+}
+
+console.log(getRandomInt(3));
+// Expected output: 0, 1 or 2
+
+console.log(getRandomInt(1));
+// Expected output: 0
+
+console.log(Math.random());
+// Expected output: a number from 0 to <1
--- src/cli.js
+++ src/cli.js
@@ -6,4 +6,5 @@ export default () => {
 
   console.log(`Hello, ${name}!`);
   return name;
-};
+} 
+

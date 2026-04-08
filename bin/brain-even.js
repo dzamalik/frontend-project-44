390bf9d75fbba1da198a80afe62e2e1dad1be646
HAYTALE <JohnDaveHarrys994@gmail.com>
2026-04-08 10:00:23 +0700

    add

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,24 +1,29 @@
 #!/usr/bin/env node
-import getName from '../src/cli.js'
+import readlineSync from 'readline-sync'
 
-export default function app() {
-  const userName = getName()
+const isEven = num => num % 2 === 0
 
- 
+export default () => {
+  console.log('Welcome to the Brain Games!')
+  const name = readlineSync.question('May I have your name? ')
+  console.log(`Hello, ${name}!`)
+  console.log('Answer "yes" if the number is even, otherwise answer "no".')
+
+  const roundsCount = 3
+  for (let i = 0; i < roundsCount; i += 1) {
+    const question = Math.floor(Math.random() * 100) + 1
+    console.log(`Question: ${question}`)
+    const answer = readlineSync.question('Your answer: ').toLowerCase()
+    const correctAnswer = isEven(question) ? 'yes' : 'no'
+
+    if (answer !== correctAnswer) {
+      console.log(`'${answer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.`)
+      console.log(`Let's try again, ${name}!`)
+      return
+    }
+
+    console.log('Correct!')
+  }
+
+  console.log(`Congratulations, ${name}!`)
 }
-
-app()
- 
-  
-function getRandomInt(max) {
-  return Math.floor(Math.random() * max);
-}
-
-console.log(getRandomInt(3));
-// Expected output: 0, 1 or 2
-
-console.log(getRandomInt(1));
-// Expected output: 0
-
-console.log(Math.random());
-// Expected output: a number from 0 to <1
